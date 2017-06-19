a9749fc0e6fa95286df89e1f9bae263bd3757a66
zhuhuilong <2991271712@qq.com>
2017-06-19 18:59:38 +0800

    变量的结构赋值

--- var/index.js
+++ var/index.js
@@ -7,3 +7,20 @@
 let [a,b,c] = [1,2,3];
 console.log(a);
 console.log(c);
+
+/*如果结构不成功，变量的值就等于undefined*/
+let[foo]=[];
+let [bar,fo]=[1];
+console.log(foo);
+console.log(bar);
+
+/*结构赋值允许默认值*/
+var [aa=true]=[];
+
+//let [x, y = 'b'] = ['a', undefined]; // x='a', y='b'
+//let [x, y = 'b'] = ['a', 'undefined'];
+
+
+//let [x = y, y = 1] = []; //ReferenceError
+
+/*对象的结构赋值*/
