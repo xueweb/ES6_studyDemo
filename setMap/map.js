5477a6bd611119c61989ee2ec1a8fe2c8ae25278
zhuhuilong <2991271712@qq.com>
2017-06-24 09:45:25 +0800

    。。。

--- setMap/map.js
+++ setMap/map.js
@@ -1,19 +1,19 @@
 /**
  * Created by Administrator on 2017/6/21 0021.
  */
-var log=require('../log');
+var log = require('../log');
 
-const map=new Map([
-    ['name','张三'],
-    ['title','Author']
+const map = new Map([
+    ['name', '张三'],
+    ['title', 'Author']
 ]);
 log(map.size)
 log(map.has('name'));
 log(map.get('title'))
 
 /*
-* 不仅仅是数组，任何具有 Iterator 接口的数据结构都可以当作Map构造函数的参数。这就是说，Set和Map都可以用来生成新的 Map。
-* */
+ * 不仅仅是数组，任何具有 Iterator 接口的数据结构都可以当作Map构造函数的参数。这就是说，Set和Map都可以用来生成新的 Map。
+ * */
 const set = new Set([
     ['foo', 1],
     ['bar', 2]
