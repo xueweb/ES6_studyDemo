673c95b3b4ca449c63ba6fb798320790b44d2ac8
zhuhuilong <zhuhuilong@zto.cn>
2018-04-23 20:02:28 +0800

    vue

--- vue/index.js
+++ vue/index.js
@@ -1,8 +1,52 @@
-/**
- * Created by PhpStorm.
- * User: Helti
- * Date: 2017/8/18
- * Time: 13:59
- */
+var emptyObj = Object.freeze({});
 
-console.log('vue index');
+var _toString = Object.prototype.toString;
+
+function toString(val) {
+    return val == null ?
+        "" :
+        typeof val === 'object' ?
+        JSON.stringify(val, null, 2) :
+        String(val)
+}
+
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ * 检测对象不是原型链上的熟悉
+ * @param {*} obj 
+ * @param {*} key 
+ */
+function hasOwn(obj, key) {
+    return hasOwnProperty.call(obj, key)
+}
+
+/**
+ * bind()
+ * @param {*} fn 
+ * @param {*} ctx 
+ */
+function polyFillBind(fn, ctx) {
+    function boundFn(a) {
+        var l = arguments.length;
+        return l ?
+            l > 1 ?
+            fn.apply(ctx, arguments) :
+            fn.call(ctx, a) :
+            fn.call(ctx)
+    }
+    boundFn._length = fn.length;
+    return boundFn;
+}
+
+function nativeBind(fn, ctx) {
+    return fn.bind(ctx);
+}
+
+var bind = Function.prototype.bind ?
+    nativeBind :
+    polyFillBind;
+
+
+console.log(undefined ==null)
+console.log('[object Object]'.slice(8,-1))
